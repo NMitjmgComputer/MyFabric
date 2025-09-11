22876c1f0fa281c556d74b3769c218c033751747
John <42098682+NMitjmgComputer@users.noreply.github.com>
2025-09-11 09:48:02 -0500

    Committing 1 item from workspace 6e3679d4-8303-4dff-9973-1e0bc727c124

---
+++ items/Developer1NB.Notebook/notebook-content.py
@@ -0,0 +1,16 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# MARKDOWN ********************
+
+# # Welcome to your new notebook
+# # Type here in the cell editor to add code!
+# Hello There
